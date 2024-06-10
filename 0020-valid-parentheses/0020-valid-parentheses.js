9619aef14a6df560d786917d55c6163e74ab05ab
Tinatin <150905727+TinatinNasirdinovna@users.noreply.github.com>
2024-06-10 12:49:40 +0600

    Time: 53 ms (79.65%), Space: 48944000 (98.14%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    let map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (map[char]) {
+            stack.push(char);
+        } 
+        else {
+            let last = stack.pop();  
+            if (map[last] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
