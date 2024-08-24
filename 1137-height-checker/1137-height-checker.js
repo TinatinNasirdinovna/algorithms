3eddcb72583eb9cdac0649b77fcbb9002f7b7d53
Tinatin <150905727+TinatinNasirdinovna@users.noreply.github.com>
2024-08-24 13:20:06 +0600

    Time: 65 ms (13.42%), Space: 48848000 (80.46%) - LeetHub

--- 1137-height-checker/1137-height-checker.js
+++ 1137-height-checker/1137-height-checker.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var heightChecker = function (heights) {
-  let sorted = [...heights].sort((a, b) => a - b);
-  let res = [];
-  for (let i = 0; i < heights.length; i++) {
-    if (heights[i] !== sorted[i]) {
-      res.push(heights[i]);
+ let count = 0
+    let sorted = [...heights].sort((a, b) => a - b);
+   for(let i = 0; i < heights.length; i++){
+    if(heights[i] !== sorted[i]) {
+      count++
     }
-  }
-  return res.length;
+   }
+    return count
 };
