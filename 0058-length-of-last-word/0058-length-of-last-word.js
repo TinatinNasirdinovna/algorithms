8f96bc6b8733d29cc8d0a41ce843f715b43c9244
Tinatin <150905727+TinatinNasirdinovna@users.noreply.github.com>
2024-08-28 17:16:16 +0600

    Time: 55 ms (35.53%), Space: 49152000 (18.02%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let arr = s.split(' ')
-    let arr2 =  arr.map(el => el ? el : null).filter(el => el !== null)
-    return arr2[arr2.length - 1].length
+   let arr2 = s.split(' ').map(el => el ? el : null ).filter(el => el !== null)
+   return arr2[arr2.length-1].length
 };
