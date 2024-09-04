3e149c52d911875d88afa4e3f73b2e242ca79e17
Tinatin <150905727+TinatinNasirdinovna@users.noreply.github.com>
2024-09-04 18:23:05 +0600

    Time: 51 ms (65.25%), Space: 48792000 (70.84%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,22 @@
  * @return {number}
  */
   var strStr = function(haystack, needle) {
-   return haystack.indexOf(needle)
+  if(haystack.length < needle.length) {
+    return -1
+  }
+  for(let i = 0; i < haystack.length; i++) {
+    let j = 0;
+    let boolFound = true 
+    while(j < needle.length) {
+        if(haystack[i+j] !== needle[j]) {
+            boolFound = false;
+            break;
+        }  
+        j++
+    }
+    if(boolFound) {
+        return i
+    }
+  }
+  return -1
   };
