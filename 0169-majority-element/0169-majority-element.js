b3ce4ddc4aa680633fb8319edd7c43a76d8ef0da
Tinatin <150905727+TinatinNasirdinovna@users.noreply.github.com>
2024-09-10 14:39:23 +0600

    Time: 55 ms (81.64%), Space: 53288000 (22.08%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -4,16 +4,9 @@
  */
 
 var majorityElement = function (nums) {
-  const objCount = {};
-  const res = Math.floor(nums.length / 2);
-  for (let num of nums) {
-      if (objCount[num] === undefined) {
-        objCount[num] = 1;
-      } else {
-          objCount[num]++;
-      }
-      if (objCount[num] > res) {
-          return num;
-      }
-  }
+    if(nums.length === 1) {
+        return nums[0]
+    }
+  return nums[Math.floor(nums.sort().length / 2)]
 };
+
