24b9f23eef1c6fdc3bd663247509991a28ef14ea
Tinatin <150905727+TinatinNasirdinovna@users.noreply.github.com>
2024-09-10 12:55:19 +0600

    Time: 96 ms (41.77%), Space: 49100000 (96.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,12 +3,12 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-      let arr = []
-      for(let i = 0; i < nums.length; i++) {
-         for(let k = i+1; k < nums.length; k++) {
-          nums[i] + nums[k]  === target ? arr.push(i,k) : arr.push()      
-         }
-      }
-    return arr
+var twoSum = function (nums, target) {
+  for(let i = 0; i < nums.length; i++) {
+    for(let j = i+1; j < nums.length; j++){
+        if(nums[i] + nums[j] === target) {
+            return [i, j]
+        }
+    }
+  }
 };
