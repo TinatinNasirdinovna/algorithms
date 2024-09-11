46bcd182774209879aa57fdfa17e6b23879c9962
Tinatin <150905727+TinatinNasirdinovna@users.noreply.github.com>
2024-09-11 10:28:20 +0600

    Time: 54 ms (84.81%), Space: 53132000 (24.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -4,9 +4,8 @@
  */
 
 var majorityElement = function (nums) {
-    if(nums.length === 1) {
-        return nums[0]
-    }
-  return nums[Math.floor(nums.sort().length / 2)]
+   nums.sort() 
+  return nums[Math.floor(nums.length / 2)]
 };
+
 
