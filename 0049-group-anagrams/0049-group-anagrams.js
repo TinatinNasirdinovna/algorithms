7a0d68ee72eb7a57ba0c551204add63a2f19f80e
Tinatin <150905727+TinatinNasirdinovna@users.noreply.github.com>
2024-11-28 15:12:42 +0600

    Time: 33 ms (70.31%), Space: 64248000 (23.30%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,14 +3,13 @@
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-  let groups = {};
-
-  strs.forEach((str) => {
-    let key = str.split("").sort().join('');
-    if(!groups[key]){
-        groups[key]= []
+  let groups = {}
+  strs.forEach(el => {
+    let key  = el.split("").sort().join()
+    if(!groups[key]) {
+        groups[key] = []
     }
-    groups[key].push(str)
-  });
+    groups[key].push(el)
+  })
   return Object.values(groups)
 };
